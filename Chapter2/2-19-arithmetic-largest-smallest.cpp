00b7a28202eb4fe7ba9dffd3afdc24c3c1f9a01f
AMuniz1 <ams.tesl@gmail.com>
2026-01-28 14:40:25 -0300

    arithmetic, largest and smallest

---
+++ Chapter2/2-19-arithmetic-largest-smallest.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+
+int main(int argc, char const **argv) {
+
+  int number1, number2, number3;
+  cout << "Enter three numbers!\n ";
+  cin >> number1 >> number2 >> number3;
+
+  int smallest = number1;
+  int largest = number1;
+  int sum {0};
+  int product{1};
+  if(number2 > largest){
+      largest = number2;
+  }
+  if (number2 < smallest){
+      smallest = number2;
+  }
+  if (number3 > largest){
+      largest = number3;
+  }
+  if (number3 < smallest){
+      smallest = number3;
+  }
+  sum = number1 + number2 + number3;
+  product = number1 * number2 * number3;
+  double average = (double)(sum)/3;
+  cout << "Sum is " << sum << "\n";
+  cout << "The average is " << average << "\n";
+  cout << "Product is "<< product << "\n";
+  cout << "The smallest is " << smallest << "\n";
+  cout << "The largest is " << largest << "\n";
+  return 0;
+}
