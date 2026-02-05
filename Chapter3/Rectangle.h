381372dc871b3327357a1055c69000623138fbb4
AMuniz1 <ams.tesl@gmail.com>
2026-02-05 17:51:58 -0300

    Not in Deitel, just an exercice of mine, training classes

---
+++ Chapter3/Rectangle.h
@@ -0,0 +1,53 @@
+#include <cmath>
+class Rectangle{
+    private:
+        int length;
+        int breadth;
+    public:
+        Rectangle(int l = 1, int b = 1){
+            setLength(l);
+            setBreadth(b);
+        }
+        Rectangle (Rectangle & rect){
+            length = rect.length;
+            breadth = rect.breadth;
+        }
+        void setLength(int l){
+            if (l < 0)
+                l = 1;
+            else
+                length = l;
+        }
+        void setBreadth(int b){
+            if (b < 0){
+                b = 1;
+
+            }
+            else {
+                breadth = b;
+            }
+        }
+        int getLength() const{
+            return length;
+        }
+        int getBreadth() const{
+            return breadth;
+        }
+        int area(){
+            return breadth * length;
+        }
+        int perimeter() {
+            return 2 * (breadth + length);
+        }
+        double diagonal() {
+            return std::sqrt(length*length + breadth*breadth);
+        }
+        bool isSquare(){
+            if (length == breadth){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+};
